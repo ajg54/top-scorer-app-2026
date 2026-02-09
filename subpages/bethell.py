5852c28e1eff5fb6d104b08c80e9f7c521617bbf
ajg54 <alexisgillett@hotmail.com>
2026-02-09 16:14:58 +0100

    Make first BEThell page

---
+++ subpages/bethell.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Jacob BEThell")
+st.header("Description")
+st.write("During the 2025/2026 Ashes Tour, Jacob Bethell finally took Ollie Pope's spot at #3 in the batting order."
+         " After that tour, Bethell's Test average stood at 43.27 based on 12 innings (including one not out)."
+         " The goal of this sub BEThell is to predict his end of year test average."
+         " The upcoming series each consist of three tests: New Zealand (home), Pakistan (home) and SA (away)."
+         " The latter series is part of the 2026/2027 tour and it is currently unclear how many tests will be in 2026.")
+
+st.header("Current Wagers")
+st.write("So far only Matt has picked a number: 40.")
