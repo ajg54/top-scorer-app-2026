6a1d0fa9a0be07b59de9c507b6d8b7ef3b4c804a
ajg54 <alexisgillett@hotmail.com>
2026-02-09 16:27:41 +0100

    Include BEThell scenario calculator

--- subpages/bethell.py
+++ subpages/bethell.py
@@ -10,3 +10,14 @@ st.write("During the 2025/2026 Ashes Tour, Jacob Bethell finally took Ollie Pope
 
 st.header("Current Wagers")
 st.write("So far only Matt has picked a number: 40.")
+
+st.header("Scenario Calculator")
+number_of_outs = st.slider("Number of Outs", min_value=0, max_value=18, value=18)
+number_of_runs = st.number_input("Number of Runs", min_value=0, max_value=2000, value=684)
+if number_of_outs == 0:
+    period_average_value = None
+else:
+    period_average_value = number_of_runs/number_of_outs
+period_average_display = st.number_input("Scenario Average (excluding previous performance)",
+                                         disabled=True, value=period_average_value)
+final_average = st.number_input("Final Average", disabled=True, value=(number_of_runs+476)/(number_of_outs+11))
